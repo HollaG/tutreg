08bf139e75ab775c0f5ca962888cb65944d4b02e
Marcus Soh <marcussoh38@gmail.com>
2023-01-09 15:52:35 +0800

    add page title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head >
+            <title> tutreg.com | Tutorial Registration Helper </title>
+        </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
