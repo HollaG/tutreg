afd20cda63b03f81df5a0ec30ff197ab4742756b
marcu <marcussoh38@gmail.com>
2023-07-03 23:08:33 +0800

    fixed bug: hashes reset

--- pages/api/users/toggleNotification.ts
+++ pages/api/users/toggleNotification.ts
@@ -4,35 +4,35 @@ import executeQuery from "../../../lib/db";
 import { BasicResponse } from "../../../types/types";
 
 interface NotificationData extends Omit<BasicResponse, "data"> {
-    data?: boolean
-};
+    data?: boolean;
+}
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<NotificationData>
 ) {
     if (req.method === "POST") {
-        const { id, hash } = req.body as { id: string, hash: string };
-       
+        const { id, hash } = req.body as { id: string; hash: string };
+
+        // TODO: validate by Hash as well
         const user = await executeQuery({
-            query: `SELECT can_notify FROM users WHERE id = ? AND hash = ?`,
-            values: [id, hash],
-        })
-        if (!user) return res.status(400).json({
-            error: "User not found!",
-            success: false
-        })
+            query: `SELECT can_notify FROM users WHERE id = ?`,
+            values: [id],
+        });
+        if (!user)
+            return res.status(400).json({
+                error: "User not found!",
+                success: false,
+            });
 
         await executeQuery({
-            query: `UPDATE users SET can_notify = ? WHERE id = ? AND hash = ?`,
-            values: [!user[0].can_notify, id, hash]
-        })
+            query: `UPDATE users SET can_notify = ? WHERE id = ?`,
+            values: [!user[0].can_notify, id],
+        });
 
         return res.status(200).json({
             success: true,
-            data: user[0] && !user[0].can_notify
-        }) 
-
-
+            data: user[0] && !user[0].can_notify,
+        });
     }
 }
